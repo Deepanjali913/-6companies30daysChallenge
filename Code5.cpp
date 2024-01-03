79b09c157799e53836ed9352e4c4ae219c652e6a
Deepanjali913 <deepanjaliaggarwal688@gmail.com>
2024-01-03 16:01:43 +0530

    Code_5 Ramdom matrix flip

---
+++ Code5.cpp
@@ -0,0 +1,51 @@
+// Random matrix flip
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    int i, j;
+    int m, n;
+
+public:
+    Solution(int m, int n)
+    {
+        i = 0;
+        j = 0;
+        this->m = m;
+        this->n = n;
+    }
+
+    vector<int> flip()
+    {
+        vector<int> ans = {i, j};
+        j = (j + 1);
+        if (j == n)
+        {
+            j = 0;
+            i += 1;
+            if (i == m)
+            {
+                i = 0;
+            }
+        }
+        return ans;
+    }
+
+    void reset()
+    {
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
+int main()
+{
+    Solution *obj = new Solution(4, 4);
+    vector<int> param_1 = obj->flip();
+    obj->reset();
+}
