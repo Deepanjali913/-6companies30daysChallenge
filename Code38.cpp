4bf541587606e5c8014fbdf081c2c96fbe052925
Deepanjali913 <deepanjaliaggarwal688@gmail.com>
2024-01-18 14:01:14 +0530

    Maximum length of repreated subarray

---
+++ Code38.cpp
@@ -0,0 +1,10 @@
+//Friends Of Appropriate Ages
+#include <bits/stdc++.h>
+using namespace std;
+int numFriendRequests(vector<int>& ages) {
+  int a[121] = {}, res = 0;
+  for (auto age : ages) ++a[age];
+  for (auto i = 15; i <= 120; ++i)
+    for (int j = 0.5 * i + 8; j <= i; ++j) res += a[j] * (a[i] - (i == j));
+  return res;
+}
